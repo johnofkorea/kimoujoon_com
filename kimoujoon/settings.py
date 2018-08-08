ea708e6b5c6907e4dfbe5586956ae5711914dbb2
John Lee <john.of.korea@gmail.com>
2018-08-09 00:02:20 +0900

    debug false

--- kimoujoon/settings.py
+++ kimoujoon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tg0r!phx9gmnf46$_()1jpk$@&p%_c9skc6qkjlj6rc=cbzehk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
