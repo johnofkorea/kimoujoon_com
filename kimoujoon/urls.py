94d4c23b5ce7b73154ac678c6af42c5b8c935a60
John Lee <john.of.korea@gmail.com>
2018-08-12 13:13:25 +0900

    home

--- kimoujoon/urls.py
+++ kimoujoon/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from english import views
+from korean import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
